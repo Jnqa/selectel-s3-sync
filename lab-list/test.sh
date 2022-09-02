f5778072640da7a23d96f7ec032dc01bd760a9ff
Jnqa <Ligez@yandex.ru>
2022-09-02 14:16:22 +0300

    fix urls

--- lab-list/test.sh
+++ lab-list/test.sh
@@ -1,4 +1,6 @@
 mkdir configs-to-upload | echo 'exists';
 for fn in `jq -r '.configs[]' configs-list.json`; do cp -r "$fn" ./configs-to-upload/; done
-array=$(find ./configs-to-upload/ -mindepth 2)
-for i in $array; do echo "https://6a39d29f-7919-4a2c-b54e-f8066f9effbb.selcdn.net/jnqa-test/configs$(echo $i | sed -e 's/.//')"; done
+pushd ./configs-to-upload/;
+array=$(find * -mindepth 1);
+for i in $array; do echo "https://502129.selcdn.ru/jnqa-test/configs/$i"; done
+popd
